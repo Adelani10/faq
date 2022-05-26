e83127d09aeb15b9b7976309c0a7619b2cd9141e
Adelani <harounadelani@gmail.com>
2022-05-26 11:33:15 +0100

    changes

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 //using selectors inside the element
 // traversing the dom
 
+const qxt = document.querySelectorAll(".questions")
 
 
-const btn = document.querySelectorAll(".btn")
 
-btn.forEach(function(bttn){
-    bttn.addEventListener("click", function(e){
-        let finalBtn = e.currentTarget.parentElement.parentElement
-        finalBtn.classList.toggle("show-text")
+
+
+qxt.forEach(function(hello){
+    const btn = hello.querySelector(".btn")
+    // console.log(btn)
+    btn.addEventListener("click", function() {
+        qxt.forEach(function(item) {
+            if (item !== hello){
+                item.classList.remove("show-text")
+            }
+        })
+        hello.classList.toggle("show-text")
     })
 })
+
+
+
+
+// const btnS = document.querySelector(".btn")
+// console.log(btnS)
 
